49fe45c8b7d8484aaf89eb990e8c092c690f358d
Christian Horst Haberler <christian@haberler.com.br>
2014-07-25 18:12:02 -0300

    Quase pronto falta alterar menu

---
+++ erros/404.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: christian
+ * Date: 24/07/14
+ * Time: 16:19
+ */
+?>
+<h1>Pagina de erro personalizada</h1>
+<hr>
+<h5>Pagina não encontrada (404)</h5>
--- index.php
+++ index.php
@@ -2,15 +2,33 @@
     require_once "template/header.php";
     require_once "template/menu.php";
 
-    if(!isset($_GET['page'])){
+    //if(!isset($_GET['page'])){
+    //    require_once("home.php");
+    //}else{
+    //    require_once($_GET['page'].".php");
+    //}
+
+$url  = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+$array = explode('/',$url['path'],3);
+$pagina = $array[2];
+
+echo $pagina;
+
+
+function rota($pagina) {
+
+    $paginasValidas = array("home","empresa","produtos","servicos","contato");
+
+    if( in_array( $pagina, $paginasValidas)):
+        require_once($pagina.".php");
+    elseif ( $pagina == ""):
         require_once("home.php");
-    }else{
-        require_once($_GET['page'].".php");
-    }
-
-
+    else:
+        require_once("erros/404.php");
+    endif;
+}
 ?>
 
-
+<?php rota($pagina); ?>
 
 <?php require_once "template/footer.php"; ?>
